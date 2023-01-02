cf591ecbfd6123aa3dab0a16034e62b1446382bf
ryanido <ryanidowu@gmail.com>
2023-01-02 15:50:17 +0000

    formatted reccomended albums and tracks

--- backend/utils.py
+++ backend/utils.py
@@ -1,8 +1,10 @@
 # Function to format info into desired format
 from collections import Counter
 import requests
+import json
+from django.http import JsonResponse, HttpResponse
 
-def artist_decon(artists):
+def top_artist_ids(artists):
     seeds = []
     genres = []
     for item in artists['items']:
@@ -12,14 +14,14 @@ def artist_decon(artists):
     return seeds[:2], [item for item, _ in Counter(genres).most_common(2)]
 
 
-def tracks_decon(tracks):
+def top_tracks_ids(tracks):
     seeds = []
     for item in tracks['items']:
         seeds.append(item['id'])
     return seeds[:2]
 
 
-def artists_genres(code):
+def get_top_artists_genres(code):
     # Set the headers for the Spotify API request
     headers = {
         'Authorization': 'Bearer {}'.format(code),
@@ -33,7 +35,7 @@ def artists_genres(code):
     #     f.write(json_string)
     # Check the status code of the response
     if response.status_code == 200:
-        artists, genres = artist_decon(json_artists)
+        artists, genres = top_artist_ids(json_artists)
         print(artists)
         print(genres)
         return artists,genres
@@ -41,7 +43,7 @@ def artists_genres(code):
         # Return an error message
         return "error","error"
 
-def tracks(code):
+def get_top_tracks(code):
     # Set the headers for the Spotify API request
     headers = {
         'Authorization': 'Bearer {}'.format(code),
@@ -55,9 +57,66 @@ def tracks(code):
     #     f.write(json_string)
     # Check the status code of the response
     if response.status_code == 200:
-        songs = tracks_decon(json_artists)
+        songs = top_tracks_ids(json_artists)
         print(songs)
         return songs
     else:
         # Return an error message
         return "error"
+
+def get_recommendations(code):
+    # Set the headers for the Spotify API request
+    headers = {
+        'Authorization': 'Bearer {}'.format(code),
+    }
+    # Make the request to the Spotify API for the user's recently played tracks
+    artists, genres = get_top_artists_genres(code)
+    songs = get_top_tracks(code)
+    params = {
+        'limit': 10,
+        'market': 'IE',
+        'seed_artists': ",".join(artists),#"4fxd5Ee7UefO4CUXgwJ7IP,3TVXtAsR1Inumwj472S9r4"
+        'seed_genres': "hip hop",#' '.join(genres),
+        'seed_tracks': ",".join(songs),
+    }
+    response = requests.get(
+        'https://api.spotify.com/v1/recommendations', headers=headers, params=params
+        )
+    print(response.url)
+    # Write the JSON string to a file
+    # json_string = json.dumps(response.json())
+    # with open('example_recommendations.json', 'w') as f:
+    #     f.write(json_string)
+    # Check the status code of the response
+    if response.status_code == 200:
+        # Return the response data as a JSON object
+        response = format_recommendations(response.json())
+        print(response)
+        return JsonResponse(response)
+    else:
+        # Return an error message
+        return HttpResponse("Failed to get recommendations", status=response.status_code)
+
+def format_recommendations(response):
+    tracks = []
+    albums = []
+    for track in response['tracks']:
+        track_info = {
+            'id': track['id'],
+            'name': track['name'],
+            'artist': track['artists'][0]['name'],
+            'cover': track['album']['images'][0]['url'],
+            'uri':track['uri']
+        }
+        album_info = {
+            'id': track['album']['id'],
+            'name': track['album']['name'],
+            'artist': track['album']['artists'][0]['name'],
+            'cover': track['album']['images'][0]['url'],
+            'uri': track['album']['uri']
+        }
+        tracks.append(track_info)
+        albums.append(album_info)
+    return {"tracks":tracks,"albums": albums}
+
+
--- backend/views.py
+++ backend/views.py
@@ -3,6 +3,7 @@ import requests
 import json
 from django.http import JsonResponse, HttpResponse
 from .utils import *
+import urllib.parse
 
 
 def recently_played(request):
@@ -16,7 +17,7 @@ def recently_played(request):
     # Make the request to the Spotify API for the user's recently played tracks
     response = requests.get(
         'https://api.spotify.com/v1/me/player/recently-played', headers=headers)
-    json_string = json.dumps(response.json())
+    
     # # Write the JSON string to a file
     # with open('example_response_recentl.json', 'w') as f:
     #     f.write(json_string)
@@ -32,31 +33,5 @@ def recently_played(request):
 def recommendations(request):
     # Get the code from the request body
     code = request.GET.get('code')
-    print(code)
-    # Set the headers for the Spotify API request
-    headers = {
-        'Authorization': 'Bearer {}'.format(code),
-    }
-    # Make the request to the Spotify API for the user's recently played tracks
-    artists, genres = artists_genres(code)
-    print("%".join(genres))
-    songs = tracks(code)
-    params = {
-        'limit': 10,
-        'seed_artists': '%'.join(artists),
-        'seed_genres': '%'.join(genres),
-        'seed_tracks': '%'.join(songs),
-    }
-    response = requests.get(
-        'https://api.spotify.com/v1/recommendations', headers=headers, params=params)
-    print(response.json())
-    # Write the JSON string to a file
-    # with open('example_response.json', 'w') as f:
-    #     f.write(json_string)
-    # Check the status code of the response
-    if response.status_code == 200:
-        # Return the response data as a JSON object
-        return JsonResponse(response.json())
-    else:
-        # Return an error message
-        return HttpResponse("Failed to get recommendations", status=response.status_code)
+    recommendations = get_recommendations(code)
+    return recommendations
